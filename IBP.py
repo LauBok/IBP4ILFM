f80ba3deee20edcf646233cb4b9ad113897eb3d2
Bo Liu <bl226@duke.edu>
2020-04-21 19:02:59 -0400

    add sampleK and initZ

--- IBP.py
+++ IBP.py
@@ -15,8 +15,19 @@ class IBP:
             self.K = Z.shape[1]
 
     def initZ(self):
+        Z = np.zeros((self.N, 0))
+        K = []
+        for i in range(self.N):
+            # the i-th customer
+            for k, nk in enumerate(K):
+                Z[i, k] = IBP.binary(nk / (i + 1))
+            K_new = np.random.poisson(self.alpha / i)
+            K += [1] * K_new
+            Z = IBP.append(Z, i, K_new)
         ## initialize self.Z and update self.K
-        pass
+        self.Z = Z
+        self.K = len(K)
+        assert(len(K) == Z.shape[1])
     
     def MCMC(self, maxiter = 1000):
         history = {
@@ -66,8 +77,22 @@ class IBP:
         res -= 1 / (2 * self.sigma_X**2) * np.trace(self.X.T @ (np.eye(self.N) - Z @ invMat @ Z.T) @ self.X)
         return res
 
-    def sampleK(self, i):
-        pass
+    def sampleK(self, i, log_thres = -6):
+        log_prob = np.array([])
+        K_new = 0
+        lmd = self.alpha / self.N
+        log_fac = lambda n: 0 if n == 0 else np.log(n) + log_fac(n - 1)
+        log_pois = lambda K: K * np.log(lmd) - lmd - log_fac(K)
+        while log_pois(K_new) > log_thres:
+            np.append(log_prob, log_pois(K_new) + self.lp(IBP.append(self.Z, i, K_new)))
+            K_new += 1
+        prob = np.exp(log_prob)
+        prob /= np.sum(prob)
+        ### sample using log_prob
+        K_post = np.argmax(np.random.multinomial(len(prob), prob, size = 1))
+        m = sum(self.Z, axis = 0) - self.Z[i, :]
+        self.Z = IBP.append(self.Z[:, m != 0], i, K_post)
+        self.K = self.Z.shape[1]
     
     def sampleAlpha(self):
         pass
